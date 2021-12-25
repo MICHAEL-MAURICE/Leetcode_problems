7e2555bce17c9c5af74f95ea54f9305e918d6453
MICHAEL-MAURICE <52583744+MICHAEL-MAURICE@users.noreply.github.com>
2021-12-26 01:53:01 +0200

    Time: 4 ms (99.56%), Space: 11.2 MB (18.19%) - LeetHub

--- two-sum/two-sum.cpp
+++ two-sum/two-sum.cpp
@@ -1,20 +1,42 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int>v;
-        for(int i=0;i<nums.size()-1;i++)
-        {
-            for(int j=i+1;j<nums.size();j++){
-                
-                if(nums[i]+nums[j]==target){
-                    
-                    v.push_back(i);
-                    v.push_back(j);
-                }
-            }
+        /*vector<pair<int,int>>num2;
+        vector<int>t;
+        for(int i=0;i<nums.size();i++){
+              pair<int,int>temp(nums[i],i);
+        
+            num2.push_back(temp);
+    
             
         }
-        return v;
+        sort(num2.begin(),num2.end());
+        int i=0;
+        int j=num2.size()-1;
         
+        while(i<j){
+            if(num2[i].first+num2[j].first==target){
+            t.push_back(num2[i].second);
+                t.push_back(num2[j].second);
+                return t;
+            }
+            else if(num2[i].first+num2[j].first>target)--j;
+            else i++;
+            }
+  return t; */
+    map<int,int>m;
+        
+    for(int i=0;i<nums.size();i++){
+        
+        auto a=m.find(target-nums[i]);
+        if(a!=m.end()){
+            
+            return{a->second,i};
+        }
+        m[nums[i]]=i;
+    }
+    return{};
+    
+    
     }
 };
