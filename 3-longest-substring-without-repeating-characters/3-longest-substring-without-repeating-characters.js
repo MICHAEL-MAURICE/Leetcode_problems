a72589b13d44cba4d65548384b8cf29d659ba026
MICHAEL-MAURICE <52583744+MICHAEL-MAURICE@users.noreply.github.com>
2022-02-08 01:15:43 +0200

    Time: 137 ms (61.95%), Space: 49.1 MB (13.04%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -3,9 +3,21 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
+    if(s.length<=1)return s.length;
     
     let longest=0;
-    for(let i=0;i<s.length;i++){
+    let left=0
+    let seen={}; let long=0;
+    
+    for(let right=0;right<s.length;right++){
+        if(seen[s[right]]>=left){
+            left=seen[s[right]]+1;
+        }
+        seen[s[right]]=right;
+        
+        longest=Math.max(longest,right-left+1);
+    }
+   /* for(let i=0;i<s.length;i++){
         let seenchar={}; let long=0;
         for(let j=i;j<s.length;j++){
             
@@ -20,6 +32,6 @@ var lengthOfLongestSubstring = function(s) {
             }
             
         }
-    }
+    }*/
     return longest;
 };
