26637b06300a3b9eb37dac06dd865910edbd84cf
MICHAEL-MAURICE <52583744+MICHAEL-MAURICE@users.noreply.github.com>
2022-02-08 22:56:48 +0200

    Time: 84 ms (87.01%), Space: 44.6 MB (34.28%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -5,7 +5,15 @@
 var isPalindrome = function(s) {
     s=s.toLowerCase().replace(/[^A-Za-z0-9]/g, "");
     
-    let t=s.split("").reverse().join("");
+    //let t=s.split("").reverse().join("");
 
-    return s==t;
+    let i=0,j=s.length-1
+    while(i<j){
+        
+        if(s[i]!=s[j]) return false;
+        i+=1;
+        j-=1;
+    }
+    return true;
+    
 };
