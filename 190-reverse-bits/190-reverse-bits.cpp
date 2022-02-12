2c0e7e383c752e988b3d27ed0a9823712d194047
MICHAEL-MAURICE <52583744+MICHAEL-MAURICE@users.noreply.github.com>
2022-02-12 19:31:36 +0200

    Time: 0 ms (100.00%), Space: 6 MB (52.00%) - LeetHub

---
+++ 190-reverse-bits/190-reverse-bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t result = 0;
+        int count = 32;
+        while (count--) {
+            result <<= 1;
+            result |= n & 1;
+            n >>= 1;
+        }
+        return result;
+    }
+};
