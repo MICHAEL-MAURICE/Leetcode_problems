30c186b106c377565345b7bce0f89fc2cb9313eb
MICHAEL-MAURICE <52583744+MICHAEL-MAURICE@users.noreply.github.com>
2022-02-19 17:05:38 +0200

    Time: 113 ms (17.24%), Space: 42.1 MB (16.78%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+     if (n == 1) {
+            return 1;
+        }
+        let dp =[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (let i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+;
